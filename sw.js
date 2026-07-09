743ffe0d558d93fa3da1c423af8c72e753826852
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-10 00:29:00 +0200

    Title up 3

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'rubikrace-v4';
+const CACHE_NAME = 'rubikrace-v5';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
