661e323b61d3e925e50323ffccff712ccd2ec64b
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-10 01:19:53 +0200

    Title up, pattern box fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'rubikrace-v7';
+const CACHE_NAME = 'rubikrace-v8';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
