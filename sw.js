422d649332b32fab1c8f45b67c4ddfd37cc9903a
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-10 12:55:04 +0200

    Title correct pattern check

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'rubikrace-v9';
+const CACHE_NAME = 'rubikrace-v1';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
