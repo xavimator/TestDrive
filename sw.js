d2ca0f9e5230d96064a8284376efa6a9d9d1ee9e
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-27 16:15:51 +0200

    Winner toast

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v13';
+const CACHE_NAME = 'mosaicchamp-v92';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
